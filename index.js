7f5ac0f22d4bfa48afa6730f216ba0b6aa8f058b
Jora <jorkabozko@gmail.com>
2020-03-13 12:13:39 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// console.log(4/0);
+// console.log('string'*2);
+
+// let something;
+// console.log(something);
+
+// let person={
+//     name:"John",
+//     age:25,
+//     isMarried:true
+// };
+// console.log(person[ "name"]);
+
+// let photos=['plum','png','orange.jpg','apple.bmp'];
+// console.log(photos[0]);
+
+// // alert("Hello World!");
+
+
+// // let answer =confirm("Are you Here?");
+// // console.log(answer);
+
+// let answer =prompt("Есть ли вам 18","Da");
+// console.log(typeof(photos));
+
+// console.log("arr"+"dsd");
+// console.log(4+"dsd");
+
+let intr=10,
+dekt=10;
+
+console.log(intr++);
+console.log(dekt--);
+console.log("2"==2);
+
+let IsChecked=true;
+let isClose=false;
+
+console.log(isClose || !IsChecked);
+
+
+ 
+
